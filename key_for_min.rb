a4907cebcae18b976d2537a271f34421e5538051
Mark Pothecary <markpothers@hotmail.com>
2019-02-13 15:44:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 current_value = 1000000000000000000
-lowest = ""
+lowest = nil
   name_hash.each do |key, value|
     if value < current_value
       lowest = key
