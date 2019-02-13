211f038116c2df1e8f7477dd471974261bf4f94d
Mark Pothecary <markpothers@hotmail.com>
2019-02-13 15:41:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+current_value = 1000000
+lowest = ""
+  name_hash do |key, value|
+    if value < current_value
+      lowest = key
+    end
+  end
+  lowest
 end
